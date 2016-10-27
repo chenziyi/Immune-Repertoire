2b022ced7e46a42ee59d8d3a7323a752061515bf
chenziyi <chziy429@163.com>
2016-10-27 13:37:17 +0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -1,1 +1,16 @@
+## Cluster of immunoglobulin variable sequence using Hamming distance
+import scipy
+import numpy
+import os
+import sys
 
+with open("" , "r") as f:
+  data=f.readlines()
+f.close()
+
+variable = f.split(",")[1]
+join= f.split(",")[2]
+family=
+counts=
+
+def cluster(seq
