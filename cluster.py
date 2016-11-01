c83a3f06da145b7c154192dade409844935ded8b
chenziyi <chziy429@163.com>
2016-11-01 15:22:30 +0800

    Update cluster.py

--- cluster.py
+++ cluster.py
@@ -32,10 +32,10 @@ def pdist(X, metric):
 # Combine the sequence with similar V gene, J gene and length of CDR3	
 cdrseq={}
 for i in range(len(aa)):
-	  if (aa[i].split("\t")[1]+aa[i].split("\t")[4]+str(len(aa[i].split("\t")[0]))) in cdrseq:
-		    cdrseq[(aa[i].split("\t")[1]+aa[i].split("\t")[4]+str(len(aa[i].split("\t")[0])))].append(aa[i])
-	  else:
-		    cdrseq[(aa[i].split("\t")[1]+aa[i].split("\t")[4]+str(len(aa[i].split("\t")[0])))]=[aa[i]]
+    if (aa[i].split("\t")[1]+aa[i].split("\t")[4]+str(len(aa[i].split("\t")[0]))) in cdrseq:
+        cdrseq[(aa[i].split("\t")[1]+aa[i].split("\t")[4]+str(len(aa[i].split("\t")[0])))].append(aa[i])
+    else:
+	cdrseq[(aa[i].split("\t")[1]+aa[i].split("\t")[4]+str(len(aa[i].split("\t")[0])))]=[aa[i]]
 
 seqcluster={}
 
@@ -47,22 +47,22 @@ def single(sequence, key, cluster):
 #    key；
 #    cluster: dictionary of cluster
     y= pdist(sequence[key], dist)
-	  Z= sch.linkage(y, method='single')
-	  T = sch.fcluster(Z, 4, 'distance')
-	  for i in range(len(T)):
-		    if key+"-"+str(T[i]) in cluster:
-			      cluster[key+"-"+str(T[i])].append(sequence[key][i])
-		    else:
-			      cluster[key+"-"+str(T[i])]=[sequence[key][i]]
+    Z= sch.linkage(y, method='single')
+    T = sch.fcluster(Z, 4, 'distance')
+    for i in range(len(T)):
+    if key+"-"+str(T[i]) in cluster:
+        cluster[key+"-"+str(T[i])].append(sequence[key][i])
+    else:
+        cluster[key+"-"+str(T[i])]=[sequence[key][i]]
 
 # Cluster analysis			
 for key in cdrseq.keys():
-	  if len(cdrseq[key])==1:
-		    seqcluster[key]=[cdrseq[key]]
-	  else:
-		    cluster={}
-		    single(cdrseq, key, cluster)
-		    seqcluster.update(cluster.items())
+    if len(cdrseq[key])==1:
+	seqcluster[key]=[cdrseq[key]]
+    else:
+        cluster={}
+	single(cdrseq, key, cluster)
+	seqcluster.update(cluster.items())
 
 if __name__=='__main__':
     main()
